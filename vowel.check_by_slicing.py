b57cf48732ffb94635697827bf059b9d82b6bc92
Arpit Panwar <86824805+Arpitsy@users.noreply.github.com>
2026-05-02 12:06:40 +0530

    Add vowel check and string manipulation logic
    if first letter is vowel check

---
+++ vowel.check_by_slicing.py
@@ -0,0 +1,13 @@
+z=input('''Write here''')
+if z[0]=="a":
+    print(z[::-1])
+elif z[0]=="e":
+    print(z[::-1])
+elif z[0]=="i":
+    print(z[::-1])
+elif z[0]=="o":
+    print(z[::-1])
+elif z[0]=="u":
+    print(z[::-1])
+else:
+    print(z.upper())
