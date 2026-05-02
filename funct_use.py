a59c664c510853c39c03aa16159db78a9a333235
Arpit Panwar <86824805+Arpitsy@users.noreply.github.com>
2026-05-02 12:21:05 +0530

    Add input handling and loop printing in funct_use.py
    Implement input checks and print statements based on user input.

---
+++ funct_use.py
@@ -0,0 +1,23 @@
+a=input(" write here")
+if a[0].lower() in "aeiou" :
+    print("ZORO THE GOAT")
+else:
+    print("LUFFY THE GOAT")
+
+a= input("Wriute Your NUmber")
+b=any(c.isdigit() for c in a)
+match a:
+    case _ if b==False :
+        print("HELLO")
+    case _:
+        print(" NOTHING")
+for i in range(0,20000):
+    print(i+1)
+for i in range(1,10,-1):
+    print(i)
+a= input("WRITE HERE")
+for i in a :
+    print(i)
+a= input("WRITE HERE")
+for i in range(len(a)):
+    print(a[i])
