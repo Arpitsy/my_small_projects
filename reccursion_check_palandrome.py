8d91f93bb0e58357fbb50fe979dce8d876c419dc
Arpit Panwar <86824805+Arpitsy@users.noreply.github.com>
2026-05-02 13:00:59 +0530

    Implement palindrome check in recursion
    Added a check to determine if a string is a palindrome and print the result.

--- reccursion_check_palandrome.py
+++ reccursion_check_palandrome.py
@@ -1,3 +1,9 @@
 def pal(a):
     if a[0]==a[-1]:
         return pal(a[1:-1])
+        
+pal("madam")    
+if len(pal())==0 or len(pal())==1:
+    print("its a palandrome no")
+else:
+    print("Its not a palandrome no")
